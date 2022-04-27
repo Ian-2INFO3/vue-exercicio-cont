65309d45a26b6782a32bc0993c7ab7281d7788bd
Ian-2INFO3 <ianrobertoestudos@gmail.com>
2022-04-27 14:12:43 -0300

    feito

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import Rodape from "@/components/MenuRodape.vue";
+import Conteudo from "@/components/MenuConteudo.vue";
+import Cabecalho from "@/components/MenuCabecalho.vue";
+export default {
+  components: { MenuLateral, Rodape, Conteudo, Cabecalho },
+};
+</script>
+<template>
+  <MenuLateral />
+  <Cabecalho />
+  <Rodape />
+  <Conteudo />
+</template>
+<style></style>
---
+++ src/components/MenuConteudo.vue
@@ -0,0 +1,19 @@
+<script>
+import NewBlock from "@/components/NewBlock.vue";
+export default {
+  components: { NewBlock },
+};
+</script>
+<template>
+  <main id="content">
+    <NewBlock />
+    <NewBlock />
+    <NewBlock />
+    <NewBlock />
+    <NewBlock class="NewBlock" />
+    <NewBlock class="NewBlock" />
+    <NewBlock class="NewBlock" />
+    <NewBlock class="NewBlock" />
+  </main>
+</template>
+<style></style>
